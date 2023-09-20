427a5b3e182a6795f41ed03e613cc3368d943d5d
zarmo17 <49298555+zarmo17@users.noreply.github.com>
2023-09-19 17:56:06 -0700

    update name

--- src/Components/Commons/Header/Header.js
+++ src/Components/Commons/Header/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
       <Navbar bg="dark" variant="dark" expand="lg">
         <Container>
           <Navbar.Brand as={Link} to={"/"}>
-            Hello Todos
+            Todos My App
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
